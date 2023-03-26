ce3f66d8d92427e5c93df1221eb425893594c30a
Allan Pinheiro <allan.hal@gmail.com>
2023-03-26 15:39:38 -0300

    atualizacao do SQL

--- aula-18-03-2023.sql
+++ aula-18-03-2023.sql
@@ -1,63 +1,282 @@
-SELECT * FROM fs08.usuario; -- Retorna as informacoes da tabela de usuarios
+------------------------------------------------------------
+-------------------------USUARIO----------------------------
+------------------------------------------------------------
+-- Criar tabela usuario
+CREATE TABLE
+	`fs08`.`usuario` (
+		`id` INT NOT NULL AUTO_INCREMENT,
+		`username` VARCHAR(45) NOT NULL,
+		`email` VARCHAR(45) NOT NULL,
+		`password` VARCHAR(45) NOT NULL,
+		PRIMARY KEY (`id`),
+		UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE, -- Não é necessario, deixei apenas como referência de estudo (MySQL Workbench criou automático)
+		UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE -- Não é necessario, deixei apenas como referência de estudo (MySQL Workbench criou automático)
+	);
+
+-- Inserindo valores iniciais na tabela usuario
+INSERT INTO
+	`fs08`.`usuario` (`username`, `email`, `password`)
+VALUES
+	('allan', 'allan@mail.com', '1234');
+
+INSERT INTO
+	`fs08`.`usuario` (`username`, `email`, `password`)
+VALUES
+	('joao', 'joao@mail.com', '1234');
+
+INSERT INTO
+	`fs08`.`usuario` (`username`, `email`, `password`)
+VALUES
+	('jose', 'jose@mail.com', '1234');
+
+-- Retorna as informacoes da tabela de usuarios
+SELECT
+	*
+FROM
+	fs08.usuario;
+
 -- id,username,email,password
--- 1,allan,asdf,asdf
--- 2,allan2,asdf21,asdf
--- 3,alaln3,asdf,32
--- 4,asdf4,asd4,asdd
+-- 1,allan,allan@mail.com,1234
+-- 2,joao,joao@mail.com,1234
+-- 3,jose,jose@mail.com,1234
+------------------------------------------------------------
+-------------------------CARRINHO---------------------------
+------------------------------------------------------------
+-- Criar tabela carrinho
+CREATE TABLE
+	`fs08`.`carrinho` (
+		`id` INT NOT NULL AUTO_INCREMENT,
+		`desconto` DECIMAL(5, 2) NULL,
+		`usuario_id` INT NULL,
+		PRIMARY KEY (`id`),
+		INDEX `chave estrangeira usuario_idx` (`usuario_id` ASC) VISIBLE, -- Não é necessario, deixei apenas como referência de estudo (MySQL Workbench criou automático)
+		CONSTRAINT `chave estrangeira usuario` FOREIGN KEY (`usuario_id`) REFERENCES `fs08`.`usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+	);
 
-select * from fs08.carrinho; -- Retorna as informacoes da tabela de carrinhos
+-- Inserindo valores iniciais na tabela carrinho
+INSERT INTO
+	`fs08`.`carrinho` (`desconto`, `usuario_id`)
+VALUES
+	(20, 3);
+
+INSERT INTO
+	`fs08`.`carrinho` (`desconto`, `usuario_id`)
+VALUES
+	(30, 2);
+
+INSERT INTO
+	`fs08`.`carrinho` (`desconto`, `usuario_id`)
+VALUES
+	(10, 4);
+
+INSERT INTO
+	`fs08`.`carrinho` (`desconto`, `usuario_id`)
+VALUES
+	(0, 1);
+
+-- Retorna as informacoes da tabela de carrinhos
+select
+	*
+from
+	fs08.carrinho;
+
 -- id,desconto,usuario_id
--- 1,20,3
--- 2,30,2
--- 3,10,4
--- 4,0,1
+-- 1,20.00,3
+-- 2,30.00,2
+-- 4,0.00,1
+------------------------------------------------------------
+-------------------------PRODUTO----------------------------
+------------------------------------------------------------
+-- Criar tabela produto
+CREATE TABLE
+	`fs08`.`produto` (
+		`id` INT NOT NULL AUTO_INCREMENT,
+		`categoria` VARCHAR(45) NULL,
+		`descricao` VARCHAR(45) NULL,
+		`valor` DECIMAL(10) NULL,
+		`img` VARCHAR(45) NULL,
+		`desconto` DECIMAL(5, 2) NULL,
+		PRIMARY KEY (`id`)
+	);
 
-select * from fs08.produto; -- Retorna as informacoes da tabela de produtos
+-- Inserindo valores iniciais na tabela produto
+INSERT INTO
+	`fs08`.`produto` (
+		`categoria`,
+		`descricao`,
+		`valor`,
+		`img`,
+		`desconto`
+	)
+VALUES
+	('tenis', 'tenis1', 100, 'http://imagem.com', 30);
+
+INSERT INTO
+	`fs08`.`produto` (
+		`categoria`,
+		`descricao`,
+		`valor`,
+		`img`,
+		`desconto`
+	)
+VALUES
+	('tenis', 'tenis2', 200, 'http://imagem.com', 30);
+
+INSERT INTO
+	`fs08`.`produto` (
+		`categoria`,
+		`descricao`,
+		`valor`,
+		`img`,
+		`desconto`
+	)
+VALUES
+	('tenis', 'tenis3', 50, 'http://imagem.com', 20);
+
+-- Retorna as informacoes da tabela de produtos
 -- id,categoria,descricao,valor,img,desconto
 -- 1,tenis,tenis1,100,1,30
 -- 2,tenis,tenis2,200,1,30
 -- 3,tenis,tenis3,50,1,20
+select
+	*
+from
+	fs08.produto;
 
-select * from fs08.carrinho_produto; -- Tabela auxiliar que guarda as informacoes de quais produtos estao em quais carrinhos, unico dado que é guardado: quantidade
--- id,carrinho_id,produto_id,quantidade
+------------------------------------------------------------
+---------------------CARRINHO_PRODUTO-----------------------
+------------------------------------------------------------
+-- Criar tabela carrinho_produto
+CREATE TABLE
+	`fs08`.`carrinho_produto` (
+		`id` INT NOT NULL AUTO_INCREMENT,
+		`carrinho_Id` INT NULL,
+		`produto_id` INT NULL,
+		`quantidade` INT NULL,
+		PRIMARY KEY (`id`),
+		INDEX `chave estrangeira carrinho_idx` (`carrinho_Id` ASC) VISIBLE, -- Não é necessario, deixei apenas como referência de estudo (MySQL Workbench criou automático)
+		INDEX `chave estrangeira produto_idx` (`produto_id` ASC) VISIBLE, -- Não é necessario, deixei apenas como referência de estudo (MySQL Workbench criou automático)
+		CONSTRAINT `chave estrangeira carrinho` FOREIGN KEY (`carrinho_Id`) REFERENCES `fs08`.`carrinho` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+		CONSTRAINT `chave estrangeira produto` FOREIGN KEY (`produto_id`) REFERENCES `fs08`.`produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+	);
+
+-- Inserindo valores iniciais na tabela carrinho_produto
+insert into
+	fs08.carrinho_produto (carrinho_id, produto_id, quantidade)
+values
+	(1, 1, 10);
+
+insert into
+	fs08.carrinho_produto (carrinho_id, produto_id, quantidade)
+values
+	(1, 2, 20);
+
+insert into
+	fs08.carrinho_produto (carrinho_id, produto_id, quantidade)
+values
+	(2, 2, 30);
+
+insert into
+	fs08.carrinho_produto (carrinho_id, produto_id, quantidade)
+values
+	(3, 1, 40);
+
+-- Tabela auxiliar que guarda as informacoes de quais produtos estao em quais carrinhos, unico dado que é guardado: quantidade
+select
+	*
+from
+	fs08.carrinho_produto;
+
+-- id,carrinho_Id,produto_id,quantidade
 -- 1,1,1,10
 -- 2,1,2,20
 -- 3,2,2,30
--- 4,3,1,40
+------------------------------------------------------------
+---------------------------SQL------------------------------
+------------------------------------------------------------
+-- Atualiza o produto com id 1 para ter o valor desconto = 30
+update fs08.produto
+set
+	desconto = 30
+where
+	id = 1;
 
--- SQL para atualizar valores
-update fs08.produto set desconto = 30 where id = 1;
-update fs08.produto set desconto = 30 where id = 2;
+-- Atualiza o produto com id 2 Spara ter o valor desconto = 15
+update fs08.produto
+set
+	desconto = 15
+where
+	id = 2;
 
--- Criar todas as ligacoes entre usuarios e carrinho (sem filtrar os usuarios e carrinhos corretos) A * B (Todas as combinacoes)
-SELECT * FROM fs08.usuario, fs08.carrinho;
+-- Exibir todas as ligacoes entre usuarios e carrinho (sem filtrar os usuarios e carrinhos corretos) A * B (Todas as combinações)
+SELECT
+	*
+FROM
+	fs08.usuario,
+	fs08.carrinho;
+
 -- As combinacoes de carrinhos que poussuem usuario;
-SELECT * FROM fs08.usuario, fs08.carrinho where usuario.id = carrinho.usuario_id;
-SELECT * FROM fs08.usuario inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id;
+SELECT
+	*
+FROM
+	fs08.usuario,
+	fs08.carrinho
+where
+	usuario.id = carrinho.usuario_id;
 
--- Combinacao do carrinho do usuario 4 (Combinacao entre tds os usuarios que possuem carrinho, mas somente do usuario 4)
-SELECT * FROM fs08.usuario inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id where usuario.id = 4;
+-- As combinacoes de carrinhos que poussuem usuario (Recomendo usar este ao invés do anterior)
+SELECT
+	*
+FROM
+	fs08.usuario
+	inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id;
 
--- Combinacao do carrinho do usuario 4 (Combinacao entre tds os usuarios que possuem carrinho, mas somente do usuario 4)
+-- Combinacao do carrinho do usuario 3 (Combinacao entre tds os usuarios que possuem carrinho, mas somente do usuario 3)
+SELECT
+	*
+FROM
+	fs08.usuario
+	inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id
+where
+	usuario.id = 3;
+
+-- Combinacao do carrinho do usuario 3 (Combinacao entre tds os usuarios que possuem carrinho, mas somente do usuario 3)
 -- traga apenas o carrinho
-SELECT carrinho_id FROM fs08.usuario inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id where usuario.id = 4;
+SELECT
+	carrinho_id
+FROM
+	fs08.usuario
+	inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id
+where
+	usuario.id = 4;
 
-
--- Insercoes de produtos nos carrinhos
-insert into fs08.carrinho_produto (carrinho_id, produto_id, quantidade) values (1, 1, 10);
-insert into fs08.carrinho_produto (carrinho_id, produto_id, quantidade) values (1, 2, 20);
-insert into fs08.carrinho_produto (carrinho_id, produto_id, quantidade) values (2, 2, 30);
-insert into fs08.carrinho_produto (carrinho_id, produto_id, quantidade) values (3, 1, 40);
-
-
--- Todos os carrinhos que possuem produtos dentro, filtrado pelo carrinho 3
+-- Os carrinhos que possuem produtos dentro, filtrado pelo carrinho 2;
 -- Somente colunas produto.id, quantidade, descricao
-select produto.id,carrinho_produto.quantidade, produto.descricao 
-	from fs08.carrinho_produto inner JOIN fs08.produto on carrinho_produto.produto_id = produto.id 
-	where carrinho_id = 3;
+select
+	produto.id,
+	carrinho_produto.quantidade,
+	produto.descricao
+from
+	fs08.carrinho_produto
+	inner JOIN fs08.produto on carrinho_produto.produto_id = produto.id
+where
+	carrinho_id = 2;
 
--- Quais os produtos, quantidade, descricao do usuario 4?
-select produto.id,carrinho_produto.quantidade, produto.descricao 
-	from fs08.carrinho_produto inner JOIN fs08.produto on carrinho_produto.produto_id = produto.id 
-	where carrinho_id = (SELECT carrinho.id FROM fs08.usuario inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id where usuario.id = 4);
-
+-- Quais os produtos, quantidade, descricao do usuario 3?
+select
+	produto.id,
+	carrinho_produto.quantidade,
+	produto.descricao
+from
+	fs08.carrinho_produto
+	inner JOIN fs08.produto on carrinho_produto.produto_id = produto.id
+where
+	carrinho_id = (
+		SELECT
+			carrinho.id
+		FROM
+			fs08.usuario
+			inner JOIN fs08.carrinho ON usuario.id = carrinho.usuario_id
+		where
+			usuario.id = 3
+	);
